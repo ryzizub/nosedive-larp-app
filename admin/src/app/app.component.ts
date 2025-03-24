00038c8028ce1d99e57c9efc804186848ec28088
David Vávra <david@vavra.me>
2025-03-24 15:30:57 +0100

    Security upgrade

--- admin/src/app/app.component.ts
+++ admin/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, Injectable, inject } from '@angular/core';
-import { Database, listVal, query, ref, push, serverTimestamp, objectVal, update } from '@angular/fire/database';
+import { Auth, signInWithCustomToken } from '@angular/fire/auth';
+import { Database, listVal, query, ref, push, serverTimestamp, objectVal, update, remove } from '@angular/fire/database';
 import { EMPTY, map } from 'rxjs';
 import { Observable } from 'rxjs/internal/Observable';
 
@@ -13,18 +14,24 @@ import { Observable } from 'rxjs/internal/Observable';
 export class AppComponent {
   title = 'admin';
   private database: Database = inject(Database);
+  private auth: Auth = inject(Auth);
   npcs: User[] = [];
   players: User[] = [];
   channels: Observable<Channel[]> = EMPTY;
   state = new State(NO_USER, new Channel("", ""), "", NO_USER, NO_USER, NO_USER, 0.05, 0.05, "", NO_USER, "", 0, NO_USER, "", "")
 
   constructor(private http: HttpClient) {
-    objectVal(ref(this.database, "config")).subscribe(config => {
-      this.state.slackBotToken = (config as Config).slackBotToken
-      this.state.feedChannelId = (config as Config).feedChannelId
-
-      this.channels = http.post<Channels>("https://slack.com/api/conversations.list?types=public_channel%2C%20private_channel", "token=" + this.state.slackBotToken, { headers: { "Content-Type": "application/x-www-form-urlencoded" } }).pipe(map(channels => channels.channels))
-    })
+    const subdomain = new URL(window.location.href).hostname.split('.')[0];
+    this.http.get<{ token: string }>('https://europe-west1-nosedive-larp.cloudfunctions.net/login?password=' + subdomain).subscribe(response => {
+      signInWithCustomToken(this.auth, response.token).then(() => {
+        objectVal(ref(this.database, "config")).subscribe(config => {
+          this.state.slackBotToken = (config as Config).slackBotToken
+          this.state.feedChannelId = (config as Config).feedChannelId
+    
+          this.channels = http.post<Channels>("https://slack.com/api/conversations.list?types=public_channel%2C%20private_channel", "token=" + this.state.slackBotToken, { headers: { "Content-Type": "application/x-www-form-urlencoded" } }).pipe(map(channels => channels.channels))
+        })
+      })
+    });
     listVal(query(ref(this.database, "nearbyUsers")), { keyField: "id" }).subscribe(users => {
       if (users != null) {
         users.push(NO_USER)
@@ -58,7 +65,7 @@ export class AppComponent {
       "Uživateli " + this.state.victim.name + " bylo sníženo hodnocení o " + this.state.penalty + "\n\nDůvod: " + this.state.reportReason + "\n\nDěkujeme uživateli " + this.state.reporter1.name + " za reportování, za odměnu bylo zvýšeno hodnocení o " + this.state.reward
       :
       "Uživateli " + this.state.victim.name + " bylo sníženo hodnocení o " + this.state.penalty + "\n\nDůvod: " + this.state.reportReason + "\n\nDěkujeme uživatelům " + this.state.reporter1.name + " a " + this.state.reporter2.name + " za reportování, za odměnu jim bylo zvýšeno hodnocení o " + this.state.reward / 2
-    this.sendSlackMessage(new User("_dive_safety", "Dive Safety", "https://firebasestorage.googleapis.com/v0/b/nosedive-larp.appspot.com/o/profile_pics%2FDive%20Safety.png?alt=media&token=1003e7ad-28fe-4093-b0f2-6cfc96bd2ee9"), this.state.feedChannelId, message)
+    this.sendSlackMessage(new User("_dive_safety", "Dive Safety", "https://firebasestorage.googleapis.com/v0/b/nosedive-larp.appspot.com/o/profile_pics%2FDive%20Safety.png?alt=media&token=1003e7ad-28fe-4093-b0f2-6cfc96bd2ee9", undefined), this.state.feedChannelId, message)
   }
 
   onResetSubmit() {
@@ -67,6 +74,24 @@ export class AppComponent {
         "slackBotToken": this.state.slackBotToken,
         "feedChannelId": this.state.feedChannelId
       })
+      this.players.forEach(player => {
+        if (player.defaultRating != undefined) {
+          update(ref(this.database, "nearbyUsers/" + player.id + "/totalRating"), {
+            "totalRating": player.defaultRating,
+            "ratingCount": 5000
+          })
+        }
+      })
+      this.npcs.forEach(npc => {
+        if (npc.defaultRating != undefined) {
+          update(ref(this.database, "nearbyUsers/" + npc.id + "/totalRating"), {
+            "totalRating": npc.defaultRating,
+            "ratingCount": npc.id == "_karolina" ? 500000 : 5000
+          })
+        }
+      })
+      // remove(ref(this.database, "reports"))
+      //  remove(ref(this.database, "ratings"))
     }
   }
 
@@ -120,12 +145,13 @@ export class User {
   constructor(
     public id: string,
     public name: string,
-    public profilePictureUrl: string
+    public profilePictureUrl: string,
+    public defaultRating: number | undefined
   ) { }
 
 }
 
-let NO_USER = new User("unknown", "-- Nikdo --", "")
+let NO_USER = new User("unknown", "-- Nikdo --", "", undefined)
 let LIVE_NPC_IDS = ["_barman", "_david", "_vaclav", "_radka"]
 
 export interface Channels {
