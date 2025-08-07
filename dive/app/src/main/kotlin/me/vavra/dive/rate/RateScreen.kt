fed85435d319427b205b4e3195341ddb01d1bbb6
David Vavra <david@vavra.me>
2025-08-07 10:05:39 +0200

    TODO: add drag handle to the top which will send rating

--- dive/app/src/main/kotlin/me/vavra/dive/rate/RateScreen.kt
+++ dive/app/src/main/kotlin/me/vavra/dive/rate/RateScreen.kt
@@ -1,7 +1,6 @@
 package me.vavra.dive.rate
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.gestures.detectVerticalDragGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -10,17 +9,11 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import me.vavra.dive.NavDestination
 import me.vavra.dive.common.ui.Avatar
 import me.vavra.dive.common.ui.StarRating
 
@@ -29,23 +22,10 @@ fun RateScreen(
     navController: NavController,
     state: RateState
 ) {
-    var swipeOffset by remember { mutableStateOf(0f) }
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .background(MaterialTheme.colorScheme.background)
-            .pointerInput(Unit) {
-                detectVerticalDragGestures { change, dragAmount ->
-                    change.consume()
-                    swipeOffset += dragAmount
-                    if (swipeOffset > 150) {
-                        navController.popBackStack()
-                    } else if (swipeOffset < -150) {
-                        navController.navigate(NavDestination.Rated(state.ratedUser.id))
-                    }
-                }
-
-            }) {
+            .background(MaterialTheme.colorScheme.background)) {
         Column(modifier = Modifier.align(Alignment.Center)) {
             Text(
                 state.currentUser.nameVokativ + ",",
