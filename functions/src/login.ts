ea5ef5a5753ea803843dae05d01ef9e0e5a51f96
David Vávra <david@vavra.me>
2025-08-16 19:17:04 +0200

    Update login.ts

--- functions/src/login.ts
+++ functions/src/login.ts
@@ -2,7 +2,7 @@ import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 
 export async function doLogin(run: String, password: string, response: functions.Response) {
-    let delegates = (await admin.database().ref("userSecrets/$run").orderByChild("password").equalTo(password).once("value")).val()
+    let delegates = (await admin.database().ref("userSecrets/"+run).orderByChild("password").equalTo(password).once("value")).val()
     if (delegates == null) {
         await delay(2000)
         sendResponse(new LoginResponse("", true), response)
